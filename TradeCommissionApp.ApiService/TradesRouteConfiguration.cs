31e923cd046f737fdde5d8d3c13b8015a77267a0
Christopher "Griz" Grzybinski <Griz@Grizcorp.com>
2024-12-03 14:27:41 -0500

    chore: Clean up Route Configurations

--- TradeCommissionApp.ApiService/TradesRouteConfiguration.cs
+++ TradeCommissionApp.ApiService/TradesRouteConfiguration.cs
@@ -7,25 +7,26 @@ public static class TradesRouteConfiguration
 {
     public static void AddTradesRoutes(this WebApplication app)
     {
+        var tradesGroup = app.MapGroup("/trades").WithTags("Trades");
+
         // Add a new trade 
         //
-        app.MapPost("/trades", async (ITradeRepository repository, TradeRequest tradeRequest) =>
+        tradesGroup.MapPost("/", async (ITradeRepository repository, TradeRequest tradeRequest) =>
         {
             var trade = tradeRequest.ToTrade();
             trade = await repository.Add(trade);
             return Results.Accepted($"/trades/{trade.Id}", trade);
 
-        }).WithTags("Trades");
+        });
 
         // Get all trades
         //
-        app.MapGet("/trades", async (ITradeRepository repository) =>
-                await repository.Get().ToListAsync())
-            .WithTags("Trades");
+        tradesGroup.MapGet("/", async (ITradeRepository repository) =>
+                await repository.Get().ToListAsync());
 
         // Get a specific trade
         //
-        app.MapGet("/trades/{id:guid}", async (ITradeRepository repository, Guid id) =>
+        tradesGroup.MapGet("/{id:guid}", async (ITradeRepository repository, Guid id) =>
         {
             var trade = await repository.Get(id);
 
@@ -33,11 +34,11 @@ public static class TradesRouteConfiguration
                 ? Results.NotFound(id)
                 : Results.Ok(trade);
 
-        }).WithTags("Trades");
+        });
 
         // Delete a fee
         //
-        app.MapDelete("/trades/{id:guid}", async (ITradeRepository repository, Guid id) =>
+        tradesGroup.MapDelete("/{id:guid}", async (ITradeRepository repository, Guid id) =>
         {
             var trade = await repository.Get(id);
             if (trade is null)
@@ -48,6 +49,6 @@ public static class TradesRouteConfiguration
             await repository.Remove(trade);
             return Results.Ok(trade);
 
-        }).WithTags("Trades");
+        });
     }
 }
