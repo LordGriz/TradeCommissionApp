11bc2b8f36058d96d0f79da73a37ab8111925611
Christopher "Griz" Grzybinski <Griz@Grizcorp.com>
2025-02-04 19:19:48 -0500

    feat: Remove Redis and output cache

--- TradeCommissionApp.AppHost/Program.cs
+++ TradeCommissionApp.AppHost/Program.cs
@@ -1,14 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache");
-
 var apiService = builder.AddProject<Projects.TradeCommissionApp_ApiService>("apiservice");
 
 var calculationService = builder.AddProject<Projects.TradeCommissionApp_CalculationService>("calculationservice")
     .WithReference(apiService);
 
 builder.AddProject<Projects.TradeCommissionApp_Web>("webfrontend")
-    .WithReference(cache)
     .WithReference(apiService)
     .WithReference(calculationService);
 
